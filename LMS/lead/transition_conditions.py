9897e01494d632148a24bb65e8808960be907733
surendhar <surendhar@blowhorn.net>
2021-07-25 16:25:47 +0530

    user fetching issue fix

--- LMS/lead/transition_conditions.py
+++ LMS/lead/transition_conditions.py
@@ -2,5 +2,6 @@ from LMS.user.models import User
 
 
 def is_sales_user(instance, user):
-    return User.objects.filter(user=user, groups__name__in=['Sales Executive (Calling team)', 'Sales Executive (Field Agent)',
+    return User.objects.filter(id=user.id, groups__name__in=['Sales Executive (Calling team)',
+                                                             'Sales Executive (Field Agent)',
                                           'Sales Manager'], is_active=True, is_staff=True).exists()
